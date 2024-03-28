f29a000b8289698e415da6f5cb3033de7b1a3c94
jtechenski <jtechenski@itba.edu.ar>
2024-03-28 14:28:27 -0300

    md5 commands

--- child.c
+++ child.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
+#include <string.h>
 // struct: file name - md5 - ID child
 
 int main(int argc, char* argv[]){
+
+    char *command = strcat("mdsum ", argv[1]);
+
+    unsigned int md5 = system(command);
+
+    /*write to shared memory:
+     *    file_name (argv[1])    md5    getpid()
+     * */
+
     return 0;
 }
