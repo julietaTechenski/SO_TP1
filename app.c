7b23397690ce3650c5e01638a7e55cf46b77434f
jtechenski <jtechenski@itba.edu.ar>
2024-03-28 14:04:05 -0300

    setting shared memory

--- app.c
+++ app.c
@@ -1,10 +1,29 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <sys/mman.h>  // for shm
 
 int main(int argc, char* argv[]){
     int children_amount = (argc-1)/10;
 
+    char *shmpath;
+//    struct shmbuf  *shmp;
 
+    if((shmpath = open("shm.txt", O_CREAT | O_RDWR | O_TRUNC, 0644)) == -1){
+        perror("Error creating shared memory file\n");
+        exit(EXIT_FAILURE);
+    }
+
+//    shmp = mmap(NULL, sizeof(*shmp), PROT_READ | PROT_WRITE,
+//                MAP_SHARED, fd, 0);
+//    if (shmp == MAP_FAILED)
+//        errExit("mmap");
+
+    int shm_fd = shm(shmpath);
+    if(shm_fd < 0){
+        perror("Error establishing shared memory access\n");
+        exit(EXIT_FAILURE);
+    }
+/*int shm_open(const char *name, int oflag, mode_t mode);*/
     for(int i = 0; i < children_amount; i++){
         int pid = fork();
         if(pid < 0){
@@ -19,4 +38,5 @@ int main(int argc, char* argv[]){
         }
     }
 
+    return 0;
 }
