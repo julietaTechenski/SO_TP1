0ac20972dbf1bb070cd3b8d9ffc6a9d844a41a4d
jtechenski <jtechenski@itba.edu.ar>
2024-03-28 13:36:09 -0300

    fork and execve children processes

--- app.c
+++ app.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
+#include <unistd.h>
 
-int main(){
-    /*
-     * code
-     * */
+int main(int argc, char* argv[]){
+    int children_amount = (argc-1)/10;
+
+
+    for(int i = 0; i < children_amount; i++){
+        int pid = fork();
+        if(pid < 0){
+            perror("Error creating child process\n");
+            exit(EXIT_FAILURE);
+        }
+        if(pid == 0){  // child process
+            char *newargv[] = {"child", argv[i]};
+            char *newenviron[] = {NULL};
+
+            execve(newargv[0], newargv,newenviron);
+        }
+    }
+
 }
