f3ec0de817dbd833c08d12dfbf255911d1733769
jtechenski <jtechenski@itba.edu.ar>
2024-03-30 19:45:36 -0300

    added fd variables to use select

--- app.c
+++ app.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/mman.h>  // for shm
+#include <sys/select.h>
 
 int main(int argc, char* argv[]){
     int children_amount = (argc-1)/10;
 
     char *shmpath;
-//    struct shmbuf  *shmp;
+//  struct shmbuf  *shmp;
 
     if((shmpath = open("shm.txt", O_CREAT | O_RDWR | O_TRUNC, 0644)) == -1){
         perror("Error creating shared memory file\n");
@@ -24,6 +25,11 @@ int main(int argc, char* argv[]){
         exit(EXIT_FAILURE);
     }
 /*int shm_open(const char *name, int oflag, mode_t mode);*/
+
+
+    fd_set rfds;
+    FD_ZERO(&rfds);
+
     for(int i = 0; i < children_amount; i++){
         int pid = fork();
         if(pid < 0){
@@ -37,6 +43,7 @@ int main(int argc, char* argv[]){
             execve(newargv[0], newargv,newenviron);
         }
     }
+
 
     return 0;
 }
