14be554c67ac11cf0ed995c574dd1b0b308f1967
Maggie Taurian <mtaurian@itba.edu.ar>
2024-03-31 16:13:02 -0300

    corrected md5 calculation child.c

--- child.c
+++ child.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+
 // struct: file name - md5 - ID child
+
+#define SIZE 32
 
 int main(int argc, char* argv[]){
 
-    char *command = strcat("mdsum ", argv[1]);
+    //Calculating md5 of file (argv[1])
+    char command[100];
+    snprintf(command, sizeof(command), "md5sum %s", argv[1]);
 
-    unsigned int md5 = system(command);
+    system(command);
 
     /*write to through pipe to parent process:
      *  file_name (argv[1] (?)) - md5 - ID child (getpid())
