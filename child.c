f08cb286984d2f50a0db0f27e01e49bf78aae283
Maggie Taurian <mtaurian@itba.edu.ar>
2024-03-31 16:14:59 -0300

    added pipe movement for output child.c

--- child.c
+++ child.c
@@ -7,11 +7,38 @@
 
 int main(int argc, char* argv[]){
 
+    int pipeFD[2];
+
+    if(pipe(pipeFD) == -1){
+        printf("ERROR: pipe() failed\n");
+        return -1;
+    }
+
+    //Saving STDOUT_FILENO before redirecting
+    int stdout = dup(STDOUT_FILENO);
+
+    dup2(pipeFD[1], STDOUT_FILENO); //stdout in writing pipe
+
     //Calculating md5 of file (argv[1])
     char command[100];
     snprintf(command, sizeof(command), "md5sum %s", argv[1]);
 
     system(command);
+
+    //Returning md5 to parent
+    char md5[SIZE];
+    read(pipeFD[0], md5, SIZE);
+
+    //Recovering stdout to send back to parent file
+    dup2(stdout, STDOUT_FILENO);
+
+    //Sending md5 of file
+    write(STDOUT_FILENO, md5, SIZE);
+    write(STDOUT_FILENO, "\n", 1);
+
+    close(stdout);
+    close(pipeFD[0]);
+    close(pipeFD[1]);
 
     return 0;
 }
