ce345db9acc83ccfa89d6422ca0e3804a459089f
Maggie Taurian <mtaurian@itba.edu.ar>
2024-04-02 19:14:34 -0300

    made new child to calculate md5 in child.c

--- child.c
+++ child.c
@@ -2,43 +2,43 @@
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/wait.h>
 
+#define MAX_LEN 4096
 #define SIZE 32
 
-int main(int argc, char* argv[]){
+int main(int argc, char* argv[]) {
 
-    int pipeFD[2];
+    //Reads file to use
+    char files[MAX_LEN];
+    ssize_t bytes_read;
 
-    if(pipe(pipeFD) == -1){
-        printf("ERROR: pipe() failed\n");
-        return -1;
+    while ((bytes_read = read(STDIN_FILENO, files, sizeof(files))) > 0) {
+        // NULL-terminated string
+        files[bytes_read] = '\0';
+
+        //Creating child to do md5
+        pid_t md5_child;
+        char *argv_chld[] = {"md5sum", files, NULL};
+        char *envp_chld[] = {NULL};
+        switch (md5_child = fork()) {
+            case -1:        //fork failed
+                printf("ERROR: fork() failed\n");
+                return -1;
+            case 0:
+                char *file = strtok(files, " \t\n");
+                //dup2(pipeFD[1], STDOUT_FILENO); //stdout in writing pipe
+                //Calculating md5 of file
+                while(file != NULL){
+                    execve("/usr/bin/md5sum", argv_chld, envp_chld);
+                    file = strtok(NULL, " \t\n");
+                }
+                return 0;
+            default:
+                wait(NULL);
+                break;
+        }
     }
-
-    //Saving STDOUT_FILENO before redirecting
-    int stdout = dup(STDOUT_FILENO);
-
-    dup2(pipeFD[1], STDOUT_FILENO); //stdout in writing pipe
-
-    //Calculating md5 of file (argv[1])
-    char command[100];
-    snprintf(command, sizeof(command), "md5sum %s", argv[1]);
-
-    system(command);
-
-    //Returning md5 to parent
-    char md5[SIZE];
-    read(pipeFD[0], md5, SIZE);
-
-    //Recovering stdout to send back to parent file
-    dup2(stdout, STDOUT_FILENO);
-
-    //Sending md5 of file
-    write(STDOUT_FILENO, md5, SIZE);
-    write(STDOUT_FILENO, "\n", 1);
-
-    close(stdout);
-    close(pipeFD[0]);
-    close(pipeFD[1]);
 
     return 0;
 }
