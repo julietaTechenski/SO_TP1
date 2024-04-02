a22746843c785ff59d3ff62246912fb1d32034d8
jtechenski <jtechenski@itba.edu.ar>
2024-04-02 19:04:12 -0300

    unlinking shm signal handler

--- view.c
+++ view.c
@@ -1,10 +1,24 @@
 #include <stdio.h>
+#include <signal.h>
 
+volatile sig_atomic_t shutdown_flag = 0;
 
-int main(){
+void shm_unlink_handler(int sig) {
+    if(sig == SIGINT){
+        shutdown_flag = 1;
+    }
+}
 
-    /*
-     * code
-     * */
+/*
+ * stdin as parameter: pathname of a shared memory object and the string that is to be copied
+ * */
+int main(int argc, char* argv[]){
+
+    signal(SIGINT, shm_unlink_handler);
+
+    while(!shutdown_flag){
+        /*printing from shared memory*/
+    }
+
     return 0;
 }
