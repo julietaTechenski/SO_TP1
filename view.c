3f0847e4187de771d1403442c3ecd87249c0743a
jtechenski <jtechenski@itba.edu.ar>
2024-04-02 19:44:35 -0300

    opening shm

--- view.c
+++ view.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 #include <signal.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/mman.h>
+#include <fcntl.h>
+
+void shm_unlink_handler(int sig);
 
 volatile sig_atomic_t shutdown_flag = 0;
 
@@ -13,8 +20,32 @@ void shm_unlink_handler(int sig) {
  * stdin as parameter: pathname of a shared memory object and the string that is to be copied
  * */
 int main(int argc, char* argv[]){
-
+    // creating signal handler for when shm is unlinked
     signal(SIGINT, shm_unlink_handler);
+
+    if(argc != 3){
+        perror("Incorrect parameters passed to view\n");
+        exit(EXIT_FAILURE);
+    }
+
+    char *shmpath = argv[1];
+    char *string = argv[2];
+    size_t len = strlen(string);
+
+    // opening shm object
+    int fd = shm_open(shmpath, O_RDONLY, 0);
+    if(fd == -1){
+       perror("View couldn't open the shm\n");
+       exit(EXIT_FAILURE);
+    }
+
+    //mapping shm into the caller's address space
+    struct shmbuf *shmp = mmap(NULL, sizeof(*shmp), PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+    if(shmp == MAP_FAILED){
+        perror("shm couldn't be mapped\n");
+        exit(EXIT_FAILURE);
+    }
+
 
     while(!shutdown_flag){
         /*printing from shared memory*/
