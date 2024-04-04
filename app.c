6da20afa31a792dd5abe53bfda6d49d59c16ace5
LucasCampoli <campolilucas@gmail.com>
2024-04-04 19:28:46 -0300

    minor details

--- app.c
+++ app.c
@@ -7,20 +7,7 @@
 #include <string.h>
 #include <stdlib.h>
 
-#define CHILD "./child"
-#define BUF_SIZE 1024
-#define READ_BUF_AUX_SIZE 64
-#define NAME_SHM "/app_shm"
-
-#define errExit(msg) {perror(msg); exit(EXIT_FAILURE);}
-
-struct shmbuf {
-    sem_t  sem_mutex;            /* POSIX unnamed semaphore */
-    sem_t  sem_read;            /* POSIX unnamed semaphore */
-    size_t index;             /* Index of reading */
-    size_t cnt;             /* Number of bytes used in 'buf' */
-    char buf[BUF_SIZE];   /* Data being transferred */
-};
+#include "head.h"
 
 int main(int argc, char* argv[]){
     if(argc == 1)
