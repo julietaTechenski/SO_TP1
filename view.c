13f974e0af3dc1a683f7fe345343d147f3f7138e
jtechenski <jtechenski@itba.edu.ar>
2024-04-05 16:15:36 -0300

    view running correctly on its own

--- view.c
+++ view.c
@@ -29,20 +29,19 @@ int main(int argc, char* argv[]){
     char shmpath[MAX_PATH_LENGTH];
     if(!isatty(fileno(stdin))){
         fgets(shmpath, MAX_PATH_LENGTH, stdin);
+    }else if(argc > 1){
+        strcpy(shmpath,argv[1]);
     }else{
-        fgets(shmpath, MAX_PATH_LENGTH, stdout);
-        printf("%s\n", shmpath);
+        errExit("Incorrect amount of parameters for view, try running with with a pipe or a shm path as parameter\n");
     }
 
     // opening shm object
 
     int fd;
 
-    if((fd = shm_open(NAME_SHM, O_CREAT | O_RDWR | O_TRUNC, 0644)) == -1)
+    if((fd = shm_open(shmpath, O_RDWR, 0644)) == -1)
     errExit("Error creating shared memory\n");
 
-    if(ftruncate(fd, sizeof(struct shmbuf)) == -1)
-    errExit("Truncate error\n");
 
     //mapping shm into the caller's address space
     struct shmbuf * shmp = mmap(NULL, sizeof(*shmp), PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
