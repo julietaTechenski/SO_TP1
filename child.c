71e9ebe5b7ec897c2c53f723fbcb0dc009688ed1
Maggie Taurian <mtaurian@itba.edu.ar>
2024-04-07 15:53:23 -0300

    deleted piping in child.c

--- child.c
+++ child.c
@@ -16,15 +16,6 @@ int main(int argc, char* argv[]) {
     //Reading file to use
     while ((bytes_read = read(STDIN_FILENO, files, sizeof(files))) > 0) {
 
-        //Creating pipes for child_md5 process
-        int pipeFD[2];
-        if(pipe(pipeFD) == -1){
-            printf("ERROR: pipe() failed\n");
-            return -1;
-        }
-
-        //Saving STDOUT_FILENO before redirecting
-        int stdout = dup(STDOUT_FILENO);
 
         //Creating child to do md5
         pid_t md5_child;
@@ -35,10 +26,6 @@ int main(int argc, char* argv[]) {
                 printf("ERROR: fork() failed\n");
                 return -1;
             case 0:
-                //Managing children piping
-                close(pipeFD[0]); // unused r-end
-                dup2(pipeFD[1], STDOUT_FILENO); //stdout in writing pipe
-                close(pipeFD[1]); // duplicate w-end
 
                 char *file = strtok(files, " \t\n");    //generate token
                 //Calculating md5 of file
@@ -48,14 +35,12 @@ int main(int argc, char* argv[]) {
                 }
                 return 0;
             default:
-                dup2(stdout, STDOUT_FILENO);    //recovers stdout
 
                 //Returning md5 to parent
                 int size = MD5_SIZE;    //md5
                 size += bytes_read;     //file name
                 size++;                 //space
                 char md5_fileName[size];
-                read(pipeFD[0], md5_fileName, size);
                 md5_fileName[size] = '\0';
 
                 //Sending md5 of file
@@ -64,10 +49,6 @@ int main(int argc, char* argv[]) {
 
                 break;
         }
-        //Closing pipes
-        close(stdout);
-        close(pipeFD[0]);
-        close(pipeFD[1]);
     }
 
     return 0;
