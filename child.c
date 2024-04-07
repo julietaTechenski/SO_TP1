5e9fdace7b86176eccc2faf277ba69c35ecc36c5
Maggie Taurian <mtaurian@itba.edu.ar>
2024-04-07 15:55:29 -0300

    modified fork() + execve() formula for popen() in child.c

--- child.c
+++ child.c
@@ -16,38 +16,17 @@ int main(int argc, char* argv[]) {
     //Reading file to use
     while ((bytes_read = read(STDIN_FILENO, files, sizeof(files))) > 0) {
 
+        //Building command
+        int size = bytes_read + sizeof(MD5SUM) + 1;
+        char command[size];
+        sprintf(command, "%s %s", MD5SUM, files);
+        command[size] = '\0';
 
-        //Creating child to do md5
-        pid_t md5_child;
-        char *argv_chld[] = {"md5sum", files, NULL};
-        char *envp_chld[] = {NULL};
-        switch (md5_child = fork()) {
-            case -1:        //fork failed
-                printf("ERROR: fork() failed\n");
-                return -1;
-            case 0:
-
-                char *file = strtok(files, " \t\n");    //generate token
-                //Calculating md5 of file
-                while(file != NULL){
-                    execve("/usr/bin/md5sum", argv_chld, envp_chld);
-                    file = strtok(NULL, " \t\n");
-                }
-                return 0;
-            default:
-
-                //Returning md5 to parent
-                int size = MD5_SIZE;    //md5
-                size += bytes_read;     //file name
-                size++;                 //space
-                char md5_fileName[size];
-                md5_fileName[size] = '\0';
-
-                //Sending md5 of file
-                printf("%s %d\n", md5_fileName, pid);
-                wait(NULL);
-
-                break;
+        //Creating pipes for child_md5 process
+        FILE * child_pipe = popen(command, READING);
+        if (child_pipe == NULL) {
+            printf("Error popen() failed");
+            return -1;
         }
     }
 
