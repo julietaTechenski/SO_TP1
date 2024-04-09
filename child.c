d51151d97f7cf7f4e1e4f0c1a688b065d4e974b0
Maggie Taurian <mtaurian@itba.edu.ar>
2024-04-09 13:29:49 -0300

    created creatingMD5Child function in child.c

--- child.c
+++ child.c
@@ -5,6 +5,29 @@
 #define MD5SUM "md5sum"
 #define READING "r"
 
+void creatingMD5Child(char * file, int file_size, char * md5){
+    //Building command
+    int size = file_size + sizeof(MD5SUM) + 1;
+    char command[size];
+    sprintf(command, "%s %s", MD5SUM, file);
+    command[size] = '\0';
+
+    //Creating pipes for child_md5 process
+    FILE * child_pipe = popen(command, READING);
+    if (child_pipe == NULL) {
+        errExit("Error popen() failed");
+    }
+
+    //Reading output
+    if (read(fileno(child_pipe), md5, MD5_SIZE) != MD5_SIZE) {
+        errExit("Error read() failed");
+    }
+    md5[MD5_SIZE] = '\0';
+
+    pclose(child_pipe);
+    return;
+}
+
 void generateAnswer(char * file, char * md5, int pid){
     char answer[MAX_LEN + MD5_SIZE + 50];
     int length = snprintf(answer, sizeof(answer), "%s - %s - %d\n", file, md5, pid);
@@ -29,26 +52,8 @@ int main(int argc, char* argv[]) {
     while ((bytes_read = read(STDIN_FILENO, files, MAX_LEN)) > 0) {
         files[bytes_read] = 0;
 
-        //Building command
-        int size = bytes_read + sizeof(MD5SUM) + 1;
-        char command[size];
-        sprintf(command, "%s %s", MD5SUM, files);
-        command[size] = '\0';
-
-        //Creating pipes for child_md5 process
-        FILE * child_pipe = popen(command, READING);
-        if (child_pipe == NULL) {
-            errExit("Error popen() failed");
-        }
-
-        //Reading output
         char md5[MD5_SIZE + 1];
-        if (read(fileno(child_pipe), md5, MD5_SIZE) != MD5_SIZE) {
-            errExit("Error read() failed");
-        }
-        md5[MD5_SIZE] = '\0';
-
-        pclose(child_pipe);
+        creatingMD5Child(files, bytes_read, md5);
 
         generateAnswer(files, md5, pid);
 
