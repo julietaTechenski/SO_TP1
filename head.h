b271efab2ec4f113f27230921e79fdbd6b280860
Maggie Taurian <mtaurian@itba.edu.ar>
2024-04-11 23:20:43 -0300

    modified design in head.h

--- head.h
+++ head.h
@@ -13,14 +13,14 @@
 #define READING "r"
 #define WRITING "w"
 
+#define BUF_SIZE 4096
 
 #define errExit(msg) {perror(msg); exit(EXIT_FAILURE);}
 
-#define BUF_SIZE 4096
 struct shmbuf{
     int cant_files_to_print;
-    sem_t  left_to_read;            /* POSIX unnamed semaphore */
-    size_t index_of_reading;             /* Index of reading */
-    size_t index_of_writing;             /* Number of bytes used in 'buf' */
-    char buf[BUF_SIZE];   /* Data being transferred */
+    sem_t  left_to_read;                /* POSIX unnamed semaphore */
+    size_t index_of_reading;            /* Index of reading */
+    size_t index_of_writing;            /* Number of bytes used in 'buf' */
+    char buf[BUF_SIZE];                 /* Data being transferred */
 };
